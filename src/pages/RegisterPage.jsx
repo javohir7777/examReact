26318c73db4e76dad17d3a45a00e6f86a97f3f46
javohir7777 <javohirjumayev7777@gmail.com>
2023-09-27 19:16:19 -0700

    hfdf

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,49 +1,8 @@
-import { toast } from "react-toastify";
 import "./Register.scss";
-import { requies } from "../server";
-import Cookies from "js-cookie";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { TOKEN } from "../container";
-import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  const { setIsAuthenticated } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (
-      (!e.target.firstname.value,
-      !e.target.lastname.value,
-      !e.target.username.value,
-      !e.target.password.value || !e.target.confirmpassword.value)
-    ) {
-      toast.error("Error");
-    } else if (e.target.password.value !== e.target.confirmpassword.value) {
-      toast.error("Error");
-    } else {
-      try {
-        const user = {
-          firstname: e.target.firstname.value,
-          lastname: e.target.lastname.value,
-          username: e.target.username.value,
-          password: e.target.password.value,
-        };
-        const {
-          data: { token },
-        } = await requies.post("auth/register", user);
-        Cookies.set(TOKEN, token);
-        setIsAuthenticated(true);
-        toast.success("Successfully registered");
-        navigate("/my-posts");
-      } catch (error) {
-        toast.error("Username or password is incorrect");
-      }
-    }
-  };
   return (
-    <div className="register" onSubmit={handleSubmit}>
+    <div className="register">
       <h1 className="register-h1">Register</h1>
       <form className="register-form">
         <input
